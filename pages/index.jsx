ff9cafbeab2efd1cab7f10aae35308abfb9bba18
RYA234 <mryuabc@gmail.com>
2023-07-20 19:51:19 +0900

    69. Create feed component and its header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
 
 export default function Home() {
   return (
@@ -18,7 +19,7 @@ export default function Home() {
 
 
         {/* Feed */}
-
+        <Feed/>
         {/* Modal */}
       </main>
     </div>
