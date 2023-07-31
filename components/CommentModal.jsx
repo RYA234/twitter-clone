65a36ed59435e92d2b167813e219a7c9b12aefca
RYA234 <mryuabc@gmail.com>
2023-07-31 21:01:41 +0900

    84. Install and implement react-modal

--- components/CommentModal.jsx
+++ components/CommentModal.jsx
@@ -34,44 +34,56 @@ export default function CommentModal(){
                 >
                     <div className="">
                         {/* 第一 */}
-                        <div className="">
+                        <div className="border-b border-gray-200 py-2 px-1.5">
                             <div
-                                onClick
-                                className=""
+                                onClick={() => setOpen(false)}
+                                className="hoverEffect w-10 h-10 flex items-center justify-center"
                             >
-                                <XIcon className="h-[23px] text-gray-700 p-0"/>
+                                <XIcon className="h-[23px] text-gray-700 p-0 "/>
                             </div>
                         </div>
 
                         {/* 第ニ  */}
-                        <div className="">
-                            <span className="" />
-                            <img />
-                            <h4 className="">
+                        <div className="p-2 flex items-center space-x-1 relative">
+                            <span className="w-0.5 h-full z-[-1] absolute left-8 top-11 bg-gray-300" />
+                            <img 
+                                className="h-11 w-11 rounded-full mr-4"
+                                src={post?.data()?.userImg}
+                                alt="user-img"
+                            />
+                            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
                                 {post?.data()?.name}
                             </h4>
-                            <span className="">
+                            <span className="text-sm sm:text-[15px]">
                                 @{post?.data()?.username} - {" "}
                             </span>
 
-                            <span className="">
+                            <span className="text-sm sm:text-[15px] hover:underline">
                                 <Moment fromNow>{post?.data()?.timesamp?.toDate()}</Moment>
                             </span>
                         </div>
-                        <p className="">
+                        <p className="text-gray-500 text-[15px] sm:text-[16px] ml-16 mb-2">
                             {post?.data()?.text}
                         </p>
 
                        
                         <div className="">
 
-                            <div className="">
-                                <img />
-                                <div>
+                            <div className="flex p-3 space-x-3">
+                                <img
+                                    src={session.user.image}
+                                    alt="user-img"
+                                    className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
+                                />
+                                <div className="w-full divide-y divide-gray-200">
                                     <div>
-                                        <textarea>
-
-                                        </textarea>
+                                        <textarea
+                                            className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
+                                            rows="2"
+                                            placeholder="Tweet your reply"
+                                            value={input}
+                                            onChange={(e) => setInput(e.target.value)}
+                                        ></textarea>
                                     </div>
                                 </div>
                         
@@ -80,15 +92,15 @@ export default function CommentModal(){
                             <div className="flex items-center justify-between pt-2.5"> 
                                 <div className="flex">
                                     <div className="">
-                                        <PhotographIcon className="h-10 w-10"/>  
+                                        <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>  
                                     </div>
-                                    <EmojiHappyIcon className="h-10 w-10"/>
+                                    <EmojiHappyIcon className="h-10 w-1 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>
                                 </div>
 
                                 <button
                                     onClick={sendComment}
                                     disabled={!input.trim()}
-                                    className=""
+                                    className="bg-blue-400 text-white px-4 dpy-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
                                 >
                                     Reply
                                 </button>
